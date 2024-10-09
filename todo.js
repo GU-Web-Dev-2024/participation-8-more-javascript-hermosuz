6be9e8830c45c409f382fc14ba8f2f3c19fb93f0
hermosuz <euzoma@zagmail.gonzaga.edu>
2024-10-09 08:34:20 -0700

    todo

--- todo.js
+++ todo.js
@@ -1,12 +1,17 @@
 "use strict";
 
 // function to add a task
-function addTask(task, taskList){
+function addTask(task, taskList) {
     const li = document.createElement("li");
     li.textContent = task;
+    taskList.appendChild(li);
 }
 
 // function to clear the input field
+function clearInput(inputField) {
+    inputField.value = "";
+}
+
 
 // alternate method to export
 
